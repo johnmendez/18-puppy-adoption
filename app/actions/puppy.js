be1b70d79d233ecacac486ffce596cb6f9ae3f4f
johnmendez <john.a.mendez2012@gmail.com>
2017-04-25 18:35:37 -0500

    Finished for now part.1

--- app/actions/puppy.js
+++ app/actions/puppy.js
@@ -1,6 +1,16 @@
 // Create an app/actions/puppy.js file
 // Create an apiUrl variable set to https://tiy-tn-class-api-fall-16.herokuapp.com/puppies/ryan (you'll update this later)
 const apiUrl = 'https://tiy-tn-class-api-fall-16.herokuapp.com/puppies/ryan';
+
+const jsonHeaders = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
+function parseJson(r) {
+  return r.json();
+}
+
 // Export a named findAllComplete function that returns an action object for PUPPY@FINDALL_COMPLETE
 export function findAllComplete(data = []) {
   return {
@@ -42,4 +52,52 @@ export function create(formData) {
      //  Dispatch createComplete with the data sent by the server
      dispatch(createComplete(puppy));
    });
+}
+
+ // Export a named findOneComplete function that returns an action object for PUPPY@FINDONE_COMPLETE
+export function findOneComplete(data = {}) {
+  return {
+    type: 'PUPPY@FINDONE_COMPLETE',
+    data,
+  };
+}
+// Export a named findOne function that takes in an argument id and returns a thunk
+export function findOne(id) {
+  //  Make a GET request to the apiUrl/id
+  return dispatch => fetch(`${apiUrl}/${id}`)
+  //  Parse json from the server response
+  .then(parseJson)
+  //  Dispatch findOneComplete with the data sent by the server
+    .then((response) => {
+      dispatch(findOneComplete(response));
+    });
+}
+// Export a named updateComplete function that returns an action object for PUPPY@FINDONE_COMPLETE
+export function updateComplete(data = {}) {
+  return {
+    type: 'PUPPY@UPDATE_COMPLETE',
+    data,
+  };
+}
+
+
+// Export a named update function that takes in arguments id and formData and returns a thunk
+export function update(id, formData) {
+  // Make a PUT request to the apiUrl/id with headers, and JSON stringified body
+  return dispatch => fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    headers: jsonHeaders,
+    body: JSON.stringify(formData),
+    // Parse json from the server response
+  }).then(parseJson)
+    .then((puppy) => {
+      // Dispatch updateComplete with the data sent by the server
+      dispatch(updateComplete(puppy));
+    });
+}
+// Export a named toggleAdopted function that takes a single argument puppy and returns a thunk
+export function toggleAdopted(puppy) {
+  //  Create a new object that spreads the old puppy object and adds a value for adopted set to the opposite of the old adopted value for the puppy
+  // Dispatch update passing in the puppy.id, and new object as arguments
+  return update(puppy.id, { ...puppy, adopted: !puppy.adopted });
 }
--- app/routes/detail.vue
+++ app/routes/detail.vue
@@ -3,48 +3,56 @@
   <div class="">
     <div class="transition-spot">
 
-    <!-- Create a form to take inputs for: (binding v-model to properties on formValues) -->
     <!-- Name (name on the server) -->
     <div class="level">
       <div class="level-item has-text-centered">
-        <p class="title" v-model="formValues.name">Name</p>
+        <p class="title">{{ currentPuppy.name }}</p>
+        <span>
+          <!-- Add a v-if to the button so that if the dog is adopted the button is Green and says "I'm Adopted" -->
+          <!-- Add a v-else to another button so that if the dog is no adopted the button is Blue and says "Adopt Me" -->
+          <!-- Both buttons should have the click listener that fires the adopt method -->
+          <button v-on:click="adopt" class="button is-primary" v-bind:class="{ 'is-success': currentPuppy.adopted }">
+           <span v-if="currentPuppy.adopted">I'm adopted!</span>
+           <span v-else>Adopt me!</span>
+         </button>
+        </span>
       </div>
     </div>
     <div class="level">
       <div class="level-item has-text-centered">
-        <img src="http://placehold.it/300x300" alt="" v-model="formValues.image_url">
+        <img v-bind:src="currentPuppy.image_url" alt="" class="image">
       </div>
     </div>
     <nav class="level is-mobile">
       <div class="level-item has-text-centered">
         <div>
           <p class="heading">AGE</p>
-          <p class="title" v-model="formValues.age">#</p>
+          <p class="title">{{ currentPuppy.age }}</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
           <p class="heading">BREED</p>
-          <p class="title" v-model="formValues.breed">Type Of Breed</p>
+          <p class="title">{{ currentPuppy.breed }}</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
           <p class="heading">COLOR</p>
-          <p class="title" v-model="formValues.color">Type Of Color</p>
+          <p class="title">{{ currentPuppy.color}}</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
           <p class="heading">SEX</p>
-          <p class="title" v-model="formValues.sex">Gender</p>
+          <p class="title">{{ currentPuppy.sex }}</p>
         </div>
       </div>
     </nav>
     <div class="card is-fullwidth">
       <div class="card-content">
       <h3 class="subtitle">About Me</h3>
-      <p v-model="formValues.description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>{{ currentPuppy.description }}</p>
     </div>
   </div>
   </div>
@@ -52,23 +60,38 @@
 </template>
 
 <script>
+import store from '../store';
+import { findOne } from '../actions/puppy';
+import { toggleAdopted } from '../actions/puppy';
 export default {
+  name: 'Detail',
   data() {
     return {
-      formValues: {
-        name: '',
-        age: '',
-        sex: '',
-        color: '',
-        breed: '',
-        image_url: '',
-        description: '',
-      }
+      // puppies should be linked to the puppies value from the redux store
+      puppies: this.$select('puppies'),
+      // currentPuppy should be a POJO (plain 'ol Javascript Object)
+      currentPuppy: null,
     };
   },
-
+  // When the detail page mounts, dispatch findOne with the id parameter from the current route
+  mounted() {
+    store.dispatch(findOne(this.$route.params.id));
+  },
+  //  Add a watch object to the component that says to watch the puppies property for changes and run a method called getPuppy
+  watch: {
+    puppies: 'getPuppy',
+    '$route.params.id': 'getPuppy',
+  },
+  // Create a getPuppy method to the methods
+ // Set the value of currentPuppy to the result of using find to find where the puppy id is equal to the id parameter in the route
   methods: {
-
+    getPuppy() {
+      this.currentPuppy = this.puppies.find(puppy => puppy.id === this.$route.params.id);
+    },
+    // Create an adopt method that dispatches the toggleAdopted action creator and passes in this.currentPuppy
+    adopt() {
+      store.dispatch(toggleAdopted(this.currentPuppy));
+    }
   },
 };
 </script>
